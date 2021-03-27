03390e9c7701e30bf64f505f29b6c2d203f5cee9
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2021-03-27 19:07:58 +0100

    Add script for security updates and toolchain

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# based on https://pythonspeed.com/articles/system-packages-docker/
+
+set -euo pipefail
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package listing
+apt-get update
+
+# Install security updates
+apt-get -y upgrade
+
+if [ "$STAGE" == "builder" ]
+then
+    # Install toolchain without unnecessary recommended packages
+    apt-get -y install --no-install-recommends apt-utils build-essential gcc
+fi
+
+# Delete cached files
+apt-get clean
+rm -rf /var/lib/apt/lists/*
