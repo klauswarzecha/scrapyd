d6ec527b8fd10b3d925e8d866758a7ce42a7ee10
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2021-04-21 19:38:03 +0200

    Add git to toolchain

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ apt-get -y upgrade
 if [ "$STAGE" == "builder" ]
 then
     # Install toolchain without unnecessary recommended packages
-    apt-get -y install --no-install-recommends apt-utils build-essential gcc
+    apt-get -y install --no-install-recommends apt-utils build-essential gcc git
 fi
 
 # Delete cached files
